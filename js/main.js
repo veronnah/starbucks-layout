a15c53c25fc40c5d13d29feb45f6bf4d832a0707
veronnah <yoo.veroon@gmail.com>
2021-04-12 21:22:04 +0200

    starbucks

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function imgSlider(image){
+    document.querySelector('.starbucks').src = image; 
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color; 
+}
