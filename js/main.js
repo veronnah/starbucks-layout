600d11e153e43c48ad6d3ededc28e7fbda689da7
veronnah <yoo.veroon@gmail.com>
2021-04-13 12:46:38 +0200

    adaptive layout burger menu created

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color; 
 }
+function toggleMenu(){
+    let menuToggle = document.querySelector('.toggle');
+    let nav = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
